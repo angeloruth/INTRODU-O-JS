1ddf922b5ee63ffa7690539286db565d3bcaffc9
angeloruth <ro081986@gmail.com>
2024-06-12 10:34:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function updateStats() {
     document.getElementById('goals').textContent = Math.floor(Math.random() * 30);
     document.getElementById('assists').textContent = Math.floor(Math.random() * 20);
 }
+
+function updateSkills() {
+    document.getElementById('speed').textContent = Math.floor(Math.random() * 100);
+    document.getElementById('dribble').textContent = Math.floor(Math.random() * 100);
+    document.getElementById('finishing').textContent = Math.floor(Math.random() * 100);
+    document.getElementById('stamina').textContent = Math.floor(Math.random() * 100);
+}
