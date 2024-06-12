a16cd59b9ce3c806836cefa53a0a4d745b61df43
angeloruth <ro081986@gmail.com>
2024-06-12 10:13:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+function updateStats() {
+    // Apenas um exemplo simples de atualização de estatísticas
+    document.getElementById('goals').textContent = Math.floor(Math.random() * 30);
+    document.getElementById('assists').textContent = Math.floor(Math.random() * 20);
+}
